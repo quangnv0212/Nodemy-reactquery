abefa0d509bff85aff66f75ea5391decab118d93
Vu Quang <quangnv.0212@gmail.com>
2023-04-15 13:20:33 +0700

    Cấu hình react-query + devtool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,27 @@
 import type { AppProps } from "next/app";
 import { Manrope } from "next/font/google";
 import axios from "axios";
-import { SWRConfig } from "swr";
-import axiosClient from "@/api/axios-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const manrope = Manrope({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={manrope.className}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
